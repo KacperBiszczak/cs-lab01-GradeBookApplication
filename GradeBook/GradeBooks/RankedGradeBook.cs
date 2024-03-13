c6981843cffccd13b90c2d878702715ab93a5d84
Kacper Biszczak <kacper.biszczak@microsoft.wsei.edu.pl>
2024-03-13 20:35:05 +0100

    Override RankedGradeBook's GetLetterGrade method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,5 +14,46 @@ namespace GradeBook.GradeBooks
             this.Type = Enums.GradeBookType.Ranked;
         }
 
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var sortedGrades = Students.OrderByDescending(s => s.AverageGrade).Select(s => s.AverageGrade).ToList();
+            int top20Percent = (int)Math.Ceiling(Students.Count * 0.2);
+            int top40Percent = (int)Math.Ceiling(Students.Count * 0.4);
+            int top60Percent = (int)Math.Ceiling(Students.Count * 0.6);
+            int top80Percent = (int)Math.Ceiling(Students.Count * 0.8);
+
+            int index = sortedGrades.FindIndex(g => g == averageGrade);
+
+            if (index < 0)
+            {
+                throw new InvalidOperationException("Student grade not found.");
+            }
+
+            if (index < top20Percent)
+            {
+                return 'A';
+            }
+            else if (index < top40Percent)
+            {
+                return 'B';
+            }
+            else if (index < top60Percent)
+            {
+                return 'C';
+            }
+            else if (index < top80Percent)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+        }
     }
 }
